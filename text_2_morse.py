28ab3edd465294078af509b4dff5bc2d6b35bce2
Abo3abed <40325801+Abo3abed@users.noreply.github.com>
2020-12-22 00:11:30 +0300

    Update text_2_morse.py
    add an exception value if letter is not in the list

--- text_2_morse.py
+++ text_2_morse.py
@@ -3,6 +3,6 @@ from encoding import ENCODERS
 
 
 print(logo)
-user_list = [letter for letter in (input('Enter text: \n').lower())] 
-morse_code = ' '.join([ENCODERS[letter] for letter in user_list])
+user_list = [letter for letter in (input('Enter text: \n').lower())]
+morse_code = ' '.join([ENCODERS[letter] if letter in ENCODERS.keys() else ENCODERS['exception'] for letter in user_list])
 print(morse_code)
