9cfb0fdc848355fc7e0ee746ec6fa3395d726ccc
Abo3abed <40325801+Abo3abed@users.noreply.github.com>
2020-12-21 23:41:43 +0300

    Update text_2_morse.py

--- text_2_morse.py
+++ text_2_morse.py
@@ -3,10 +3,6 @@ from encoding import ENCODERS
 
 
 print(logo)
-
-user_input = input('Enter text: \n').lower()
-user_list = [letter for letter in user_input]
-morse_code_list = [ENCODERS[letter] for letter in user_list]
-morse_code = ' '.join(morse_code_list)
-
+user_list = [letter for letter in (input('Enter text: \n').lower())] 
+morse_code = ' '.join([ENCODERS[letter] for letter in user_list])
 print(morse_code)
